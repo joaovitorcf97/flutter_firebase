ddc2a2645cfc559004ad704f8fa84fbf4d558dac
Joao Vitor <joaovitorcfdev@gmail.com>
2022-03-06 14:25:48 -0300

    Configuring notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/firebase_messaging/custom_firebase_messaging.dart';
 
 import 'pages/home_page.dart';
 
-void main() {
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  await CustomFirebaseMessaging().inicialize();
+
+  await CustomFirebaseMessaging().getTokenFirebase();
+
   runApp(const MyApp());
 }
 
@@ -14,10 +25,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      navigatorKey: navigatorKey,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (_) => const HomePage(),
+        '/virtual': (_) => Scaffold(
+              appBar: AppBar(),
+              body: const SizedBox.expand(
+                child: Center(
+                  child: Text('Virtual Page'),
+                ),
+              ),
+            ),
+      },
     );
   }
 }
